6092cadf12cf7c7a57e14965e1e01f535b6d9f65
LeonZhu <1933832881@qq.com>
2018-12-30 22:58:36 +0800

    增加表

--- model.py
+++ model.py
@@ -1,7 +1,18 @@
 from orm import orm
+<<<<<<< HEAD
 import asyncio
 
 class Templ(orm.Model):
     __tablename__ = 'emacs'
     id = orm.Integer(length=11)
     name = orm.String(length=32)
+=======
+
+
+
+class Templ(orm.Model):
+    __tablename__ = 'emacs'
+    id = orm.Integer()
+    name = orm.String(legth=32)
+
+>>>>>>> 增加表
