b98f50d295fd739fce8aed739b72ce9b09a514a1
LeonZhu <1933832881@qq.com>
2018-12-30 22:59:59 +0800

    增加表

--- model.py
+++ model.py
@@ -1,18 +1,21 @@
 from orm import orm
-<<<<<<< HEAD
-import asyncio
-
-class Templ(orm.Model):
-    __tablename__ = 'emacs'
-    id = orm.Integer(length=11)
-    name = orm.String(length=32)
-=======
-
-
 
 class Templ(orm.Model):
     __tablename__ = 'emacs'
     id = orm.Integer()
     name = orm.String(legth=32)
 
->>>>>>> 增加表
+
+class LiteMailFootPrints(orm.Model):
+	__tablename__ = 'litem_foot_prints'
+	id = orm.Integer(primay_key=True)
+	user_id = orm.Integer()
+	goods_id = orm.Integer()
+	add_time = orm.Datetime()
+	update_at = orm.Datetime()
+	deleted_at = TinyInteger()
+
+class LiteMailAdmin(orm.Model)
+	__tablename__ = 'litem_foot_prints'
+	id = orm.Integer(primay_key=True)
+	user_name = orm.String(length = 63)
