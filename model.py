e2bac8a41c9c5692b21431abe2f7d22410ccc9c6
LeonZhu <1933832881@qq.com>
2019-01-01 16:48:36 +0800

    增加表

--- model.py
+++ model.py
@@ -13,9 +13,33 @@ class LiteMailFootPrints(orm.Model):
 	goods_id = orm.Integer()
 	add_time = orm.Datetime()
 	update_at = orm.Datetime()
-	deleted_at = TinyInteger()
+	deleted_at = orm.TinyInteger()
 
 class LiteMailAdmin(orm.Model)
 	__tablename__ = 'litem_foot_prints'
 	id = orm.Integer(primay_key=True)
-	user_name = orm.String(length = 63)
+	username = orm.String(length = 63)
+	password = orm.String(length = 63)
+	last_login_ip = orm.String(length = 63)
+	last_login_time = orm.Datetime()
+	avatar = orm.String(length = 255)
+	add_time = orm.Datetime()
+	update_at = orm.Datetime()
+	deleted_at = orm.TinyInteger()
+
+class LiteMailAddress(orm.Model)
+	__tablename__ = 'litem_foot_prints'
+	id = orm.Integer(primay_key=True)
+	name = orm.String(length = 63)
+	user_id = orm.Integer()
+	address = orm.String(length = 127)
+	mobile = orm.String(length = 20)
+	is_default = orm.TinyInteger()
+	add_time = orm.Datetime()
+	update_at = orm.Datetime()
+	deleted_at = TinyInteger()
+
+class LiteMailGoodsSpecification(orm.Model)
+	__tablename__ = 'litem_foot_prints'
+	id = 
+
